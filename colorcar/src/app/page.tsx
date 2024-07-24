c5c90dc1be83000d27179721c976daf050040f8c
gdkvntn <73638301+gdkvntn@users.noreply.github.com>
2024-07-24 11:15:51 +0300

    change slider

--- colorcar/src/app/page.tsx
+++ colorcar/src/app/page.tsx
@@ -1,18 +1,18 @@
+import DemoSlider from "./ui/components/SliderSwiper";
 import Header from "./ui/header/Header";
 import InfoSection from "./ui/landing/InfoSection/InfoSection";
-import DemoSlider from "./ui/utils/SliderSwiper";
 
 import dataSlider from "@/app/_data/slider-data.json";
 
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
       <main className="max-w-6xl m-auto">
         <div>
           <DemoSlider data={dataSlider} />
         </div>
-        <InfoSection/>
+        <InfoSection />
       </main>
     </>
   );
--- colorcar/src/app/ui/components/SliderSwiper.tsx
+++ colorcar/src/app/ui/components/SliderSwiper.tsx
@@ -8,7 +8,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
-
 interface Slide {
   id: number;
   title: string;
@@ -27,64 +26,60 @@ interface DemoSliderProps {
 }
 
 interface ButtonProps {
-    id: number;
-    text: string;
-    link: string;
-    type: string;
-  }
-
+  id: number;
+  text: string;
+  link: string;
+  type: string;
+}
 
 const SliderButtons: React.FC<{ buttons: ButtonProps[] }> = ({ buttons }) => {
-    return buttons.map(({ id, link, text }) => (
-      <a target="_blank" key={id} href={link}>
-        <span>{text}</span>
-      </a>
-    ));
+  return buttons.map(({ id, link, text }) => (
+    <a target="_blank" key={id} href={link}>
+      <span>{text}</span>
+    </a>
+  ));
 };
-
 
 const DemoSlider: React.FC<DemoSliderProps> = ({ data }) => {
   return (
-    <section className="w-ful h-[32rem]">
-      <div className=" h-screen">
-        <ul className="h-full w-full">
-          <Swiper
-            pagination={{ type: "bullets", clickable: true }}
-            autoplay={true}
-            loop={true}
-            modules={[Autoplay, Navigation, Pagination]}
-          
-          >
-            {data.map(({ id, image, tagline, title, buttons }) => (
-              <SwiperSlide key={id}>
-                <div
-                  className="h-full w-full absolute left-0 top-0 "
-                  style={{
-                    background: `url(${image}) center center / cover scroll no-repeat`,
-                  }}
-                ></div>
-                <div className="h-full w-full absolute left-0 top-0 bg-black opacity-20"></div>
-                <div className="relative z-10 h-full flex items-center justify-center">
-                  <div className="absolute left-4 top-1/3">
-                    {tagline && (
-                      <p className="text-md sm:text-xl lg:text-3xl font-semibold text-white">
-                        {tagline}
-                      </p>
-                    )}
-                    <p className="text-3xl sm:text-6xl lg:text-8xl font-bold text-white">
-                      {title}
+    <section className="w-full">
+      <div className="h-full w-full">
+        <Swiper
+          pagination={{ type: "bullets", clickable: true }}
+          autoplay={true}
+          loop={true}
+          modules={[Autoplay, Navigation, Pagination]}
+          className="h-[620px]"
+        >
+          {data.map(({ id, image, tagline, title, buttons }) => (
+            <SwiperSlide key={id}>
+              <div
+                className="h-full w-full absolute left-0 top-0 "
+                style={{
+                  background: `url(${image}) center center / cover scroll no-repeat`,
+                }}
+              ></div>
+              <div className="h-full w-full absolute left-0 top-0 bg-black opacity-20"></div>
+              <div className="relative z-10 h-full flex items-center justify-center">
+                <div className="absolute left-4 top-1/3">
+                  {tagline && (
+                    <p className="text-md sm:text-xl lg:text-3xl font-semibold text-white">
+                      {tagline}
                     </p>
-                    {buttons.length > 0 ? (
-                      <p className=" bg-gray-800 inline-block px-9 py-2 rounded-full text-white mt-10 lg:mt-20">
-                        <SliderButtons buttons={buttons} />
-                      </p>
-                    ) : null}
-                  </div>
+                  )}
+                  <p className="text-3xl sm:text-6xl lg:text-8xl font-bold text-white">
+                    {title}
+                  </p>
+                  {buttons.length > 0 ? (
+                    <p className=" bg-gray-800 inline-block px-9 py-2 rounded-full text-white mt-10 lg:mt-20">
+                      <SliderButtons buttons={buttons} />
+                    </p>
+                  ) : null}
                 </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </ul>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
