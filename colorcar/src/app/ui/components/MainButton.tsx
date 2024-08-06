53edf761456055f753b9d720d05d17c4d11b36b5
Uttake <88742186+Uttake@users.noreply.github.com>
2024-08-06 14:55:18 +0300

    button

--- colorcar/src/app/ui/components/MainButton.tsx
+++ colorcar/src/app/ui/components/MainButton.tsx
@@ -1,14 +1,18 @@
+import clsx from 'clsx'
 import React from 'react'
 
 type MainButtonType = {
   title: string,
-  fontSize: string
+  fontSize: string,
+  color: string,
+  maxW: string,
+  hgt: string
 }
 
-const MainButton = ({title, fontSize} : MainButtonType) => {
+const MainButton = ({title, fontSize, color, maxW, hgt} : MainButtonType) => {
   return ( 
-    <button className='px-5 py-3'>
-      <span className={fontSize}>
+    <button className={clsx(maxW, 'border-4 border-[#d42e12] w-full', hgt)}>
+      <span className={clsx(fontSize, color)}>
         {title}
       </span>
     </button>
