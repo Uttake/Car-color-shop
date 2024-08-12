de4e4ebda3acdf04b41bf5cda9993679b6576a35
Uttake <88742186+Uttake@users.noreply.github.com>
2024-08-12 15:38:11 +0300

    edit supabase

--- colorcar/src/app/ui/components/FormAdmin.tsx
+++ colorcar/src/app/ui/components/FormAdmin.tsx
@@ -13,7 +13,8 @@ export default function FormAdmin() {
       <Input name="name" label="Название" placeholder="Краска" />
       <Input name="description" label="Описание" placeholder="Краска желтая" />
       <Input name="price" label="Цена" placeholder="20 byn" />
-      <input type="file" />
+      <label htmlFor="image">Картинка</label>
+      <input type="file" name="image" />
       <ButtonForm />
     </form>
   );
--- colorcar/src/app/utils/actions.ts
+++ colorcar/src/app/utils/actions.ts
@@ -1,12 +1,41 @@
 "use server";
 
 import { supabase } from "./supabaseClient";
+import { v1 } from "uuid";
 
 export const postData = async (formData: FormData) => {
   const title = formData.get("name");
   const description = formData.get("description");
   const price = Number(formData.get("price"));
+  const image = formData.get('image');
+  
+  let id = v1();
+  try {
+    if(image) {
+      const { data: storageData, error: storageError } = await supabase
+      .storage
+      .from('Products images')
+      .upload(`${id}.png`, image, {
+      cacheControl: '3600',
+      upsert: false
+    })
+    }
+      // const { data: storageImageUrl} = await supabase
+      // .storage
+      // .from('Products images')
+      // .getPublicUrl(`${id}.png`)
 
-  let { data: Products, error } = await supabase.from("Products").select("*");
-  console.log(Products);
-};
+    const { data, error } = await supabase
+    .from('Products')
+    .insert([
+      {id, title, description, images: `https://invnbdbustikwbnttmdr.supabase.co/storage/v1/object/public/Products%20images/${id}.png`, price},
+    ])
+    .select()
+    
+    if(error) {
+      throw error
+    }
+  } catch (err) {
+      console.log(err)
+  }
+}
